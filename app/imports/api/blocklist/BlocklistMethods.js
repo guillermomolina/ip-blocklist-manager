58854a44af010e62964597a3f9d877737a9f8c55
gmolina <gmolina@base.cat>
2021-10-14 12:00:14 +0200

    Removed blocklists.insert method

--- app/imports/api/blocklist/BlocklistMethods.js
+++ app/imports/api/blocklist/BlocklistMethods.js
@@ -14,17 +14,6 @@ Meteor.methods({
         IpAddressCollection.remove({ blocklist_id: blocklist_id });
         BlocklistCollection.remove(blocklist_id);
     },
-    'blocklists.insert': function (blocklist) {
-        BlocklistSchema.validate(blocklist);
-
-        if (BlocklistCollection.find({name: blocklist.name}).count() != 0) {
-            throw new Meteor.Error('blocklists.insert.blocklist-exists',
-                `The list "${blocklist.name}" is already present. Choose another name`);
-        }
-
-        delete blocklist._id;
-        return BlocklistCollection.insert(blocklist);
-    },
     'blocklists.isNameAvailable': function (name) {
         check(name, String);
 
