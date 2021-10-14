59ab6c44b7464e0a813593ed001477e3b343d204
gmolina <gmolina@base.cat>
2021-10-14 11:33:10 +0200

    Change text /list url to /list.txt

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## Version 0.1.2
+
+- Added blocklists.insert method (prevent duplicates)
+- Added validation="keyup" to autoForm's
+- Update accounts-ui to 1.4.1
+- Added aldeed:schema-index (3.0.0) for index and unique mongo values
+- Change text /list url to /list.txt
+
 ## Version 0.1.1
 
 - Added Version information
@@ -7,3 +15,5 @@
 - Added delete buttons to edit pages
 
 ## Version 0.1.0 (Initial release)
+
+- Added ipAddresses.insert method (prevent duplicates)
--- app/imports/startup/server/router.js
+++ app/imports/startup/server/router.js
@@ -1,7 +1,7 @@
 import { Picker } from 'meteor/meteorhacks:picker';
 import { IpAddressCollection } from '../../api/ipaddress/IpAddressCollection';
 
-Picker.route('/:_blocklist_id/list', function (params, request, response, next) {
+Picker.route('/:_blocklist_id/list.txt', function (params, request, response, next) {
   const ipAddressList = IpAddressCollection.find({ blocklist_id: params._blocklist_id }).fetch().map(o => o.address);
 
   response.setHeader('Content-Type', 'text/plain');
